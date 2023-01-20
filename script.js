921f070027c98703e77210ac625c1f274cc5eb34
Muradova Farangiz [FE16] <102415848+farangiztae@users.noreply.github.com>
2023-01-20 16:32:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ console.log(fruit);
 
 let academy = "Open Web";
 consolo.log(academy);
+
+let name = "Alex";
+alert(name);
+
+let age = 25;
+alert(age);
+
+let computer = "Asus";
+consolo.log(computer)
